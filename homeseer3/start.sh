ad9d3c6861cf1419266829c9106afb2943cfbfa0
Jonathan Bradshaw <bradsjm@users.noreply.github.com>
2015-12-27 13:21:05 -0600

    Create start.sh

---
+++ homeseer3/start.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Get current version
+[ -f /HomeSeer/version.txt ] && VERSION=$(cat /HomeSeer/version.txt)
+
+# Get remote version
+HS_URL=$(curl -sL http://homeseer.com/current-downloads.html | grep -Po 'http://homeseer.com/updates3/hs3_linux_3[0-9_.]+.tar.gz')
+
+# Download and untar if versions are not the same
+if [ "$VERSION" != "$HS_URL" ]; then
+  echo "Downloading $HS_URL ..."
+  mkdir -p /HomeSeer
+  wget -qO - "${HS_URL}" | tar -C /HomeSeer -zx --strip-components 1
+  echo "$HS_URL" > /HomeSeer/version.txt
+fi
+
+# Execute
+
+cd /HomeSeer
+export LANG=en_US.UTF-8
+exec mono HSConsole.exe --log
