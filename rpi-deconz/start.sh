6070b0b9bf4b69e103fa02648554ec78175d2333
Jonathan Bradshaw <bradsjm@users.noreply.github.com>
2016-01-02 16:10:50 -0600

    Update start.sh

--- rpi-deconz/start.sh
+++ rpi-deconz/start.sh
@@ -4,4 +4,5 @@ echo $PASS | vncpasswd -f > /root/.vncpasswd
 rm -f /tmp/.X1-lock
 Xtightvnc -rfbauth /root/.vncpasswd -geometry 1024x768 -depth 24 :1 &
 x-window-manager &
+GCFFlasher -r
 exec deCONZ-autostart.sh
