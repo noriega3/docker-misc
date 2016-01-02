fec5af3d479dc855f70335e2bb7a18330db02d56
Jonathan Bradshaw <bradsjm@users.noreply.github.com>
2016-01-02 14:29:24 -0600

    Update start.sh

--- rpi-deconz/start.sh
+++ rpi-deconz/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-Xvnc -rfbauth=/root/.vnc/passwd -geometry 1024x768 :1 &
+PASS=${PASS:-"deconz"}
+echo $PASS | vncpasswd -f > /root/.vncpasswd
+Xvnc -rfbauth=/root/.vncpasswd -geometry 1024x768 :1 &
 xsetroot -solid grey
 x-window-manager &
 exec deCONZ --auto-connect=1 --http-port=80
