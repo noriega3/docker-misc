5f1c8f4823a8023ec50a2797a7216930a89c1824
Jonathan Bradshaw <bradsjm@users.noreply.github.com>
2016-01-02 15:39:17 -0600

    Update start.sh

--- rpi-deconz/start.sh
+++ rpi-deconz/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-PASS=${PASS:-"deconz"}
+PASS=${PASS:-"delight"}
 echo $PASS | vncpasswd -f > /root/.vncpasswd
 Xtightvnc -rfbauth /root/.vncpasswd -geometry 1024x768 -depth 24 :1 &
 x-window-manager &
