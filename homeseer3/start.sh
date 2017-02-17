7e037bee0677c03224d6653e289ea3c00530cc29
Jonathan Bradshaw <bradsjm@users.noreply.github.com>
2017-02-16 20:34:25 -0600

    Update start.sh

--- homeseer3/start.sh
+++ homeseer3/start.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+trap exit 0 SIGTERM
+export LANG=en_US.UTF-8
+
+TIMEZONE=${TIMEZONE:-"America/Los_Angeles"}
+echo $TIMEZONE > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
 
 # Get current version
 [ -f /HomeSeer/version.txt ] && VERSION=$(cat /HomeSeer/version.txt)
@@ -14,8 +20,12 @@ if [ "$VERSION" != "$HS_URL" ]; then
   echo "$HS_URL" > /HomeSeer/version.txt
 fi
 
-# Execute
+chown -R root:root /HomeSeer
 
+# bug fix for case sensitive filesystems
+# without this myhs.homeseer.com wont load icons
+ln -s /HomeSeer/html/images/homeseer /HomeSeer/html/images/HomeSeer
+
+# Execute
 cd /HomeSeer
-export LANG=en_US.UTF-8
 exec mono HSConsole.exe --log
