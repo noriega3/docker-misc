bb58108ba7eae94012826f83620a35d6dcfea15e
Jonathan Bradshaw <jonathan@Jonathans-MacBook-Pro.local>
2017-05-06 19:46:11 -0500

    Updated Direwolf and moved to Alpine base

--- rpi-direwolf/start.sh
+++ rpi-direwolf/start.sh
@@ -1,21 +1,18 @@
 #! /bin/sh
-
-set -e
-
 cd /root
 
 # Update configuration using environment variables
 if [ -n "$MYCALL" ]; then
-  sed -i 's|^MYCALL.*$|MYCALL '"$MYCALL"'|g' direwolf.conf
+  sed -i 's|^MYCALL.*$|MYCALL '"$MYCALL"'|g' sdr.conf
 fi
 
 if [ -n "$IGSERVER" ]; then
-  sed -i 's|^IGSERVER.*$|IGSERVER '"$IGSERVER"'|g' direwolf.conf
+  sed -i 's|^IGSERVER.*$|IGSERVER '"$IGSERVER"'|g' sdr.conf
 fi
 
 if [ -n "$IGLOGIN" ]; then
-  sed -i 's|^#IGLOGIN.*$|IGLOGIN '"$IGLOGIN"'|g' direwolf.conf
+  sed -i 's|^#IGLOGIN.*$|IGLOGIN '"$IGLOGIN"'|g' sdr.conf
 fi
 
 # Start Direwolf
-rtl_fm -f 144.39M -g 50 - | direwolf -n 1 -r 24000 -t 0 -D 1 -
+rtl_fm -f $FREQUENCY ${*} - | /usr/local/bin/direwolf -c sdr.conf -r 24000 -D 1 -a 100 -
