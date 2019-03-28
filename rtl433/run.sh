ced56e06fddea43806249d7479c4421d782da13f
Jonathan Bradshaw <jonathan@DanielDoszsiPad.localdomain>
2019-03-28 19:57:57 -0400

    Updated

--- rtl433/run.sh
+++ rtl433/run.sh
@@ -3,5 +3,4 @@
 set -o errexit         # Exit on most errors (see the manual)
 set -o xtrace          # Trace the execution of the script (debug)
 
-CMD="/usr/local/bin/rtl_433 -q -U -F json $@"
-exec /usr/bin/socat TCP-LISTEN:${PORT:-9433},reuseaddr,fork SYSTEM:"${CMD}",pty
+/usr/local/bin/rtl_433 $@ | socat STDIN TCP-LISTEN:${PORT:-9433},reuseaddr,fork
