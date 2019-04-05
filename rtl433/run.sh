d6d3451df78ca449a0409dbaf9cf5e3e55e6253b
Jonathan Bradshaw <jonathan@JBMacBook.home>
2019-04-05 15:36:34 -0400

    Updated

--- rtl433/run.sh
+++ rtl433/run.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-
-set -o errexit         # Exit on most errors (see the manual)
-set -o xtrace          # Trace the execution of the script (debug)
-
-/usr/local/bin/rtl_433 $@ | socat STDIN TCP-LISTEN:${PORT:-9433},reuseaddr,fork
+exec /usr/local/bin/rtl_433 $@ | \
+     /usr/bin/socat STDIN TCP-LISTEN:${PORT:-9433},reuseaddr,fork
